43154e8768e79a5e059e6eb296bb3e822b22bbba
CyrusTheHedgehog <immanuelbask@gmail.com>
2017-01-21 17:35:27 +0000

    Add Interface Method

---
+++ interface.py
@@ -0,0 +1,10 @@
+
+def inputNode():
+    illness = input("Please type in Illness\n").lower()
+    print(illness)
+    symptoms = input("Please type in symptoms, separated by commas\n").split(",")
+    symptoms = [symptom.strip().lower() for symptom in symptoms]
+    print(symptoms)
+    cure = input("Please type in cure\n").lower()
+    print(cure)
+    return illness, symptoms, cure
